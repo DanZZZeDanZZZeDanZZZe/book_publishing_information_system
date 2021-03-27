51251f26057ecb64329b16dc3615067aef2cc3c1
Artem Grankin <artem_grankin@epam.com>
2021-03-27 20:29:55 +0300

    feature: add models

---
+++ models/Author.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const AuthorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  patronymic: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
+  },
+  books: [
+    {
+      type: mongoose.ObjectId,
+      ref: 'Book',
+      autopopulate: true,
+    },
+  ],
+  owner: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+})
+
+AuthorSchema.plugin(require('mongoose-autopopulate'))
+
+const Author = mongoose.model('Author', AuthorSchema)
+
+module.exports = Author
---
+++ models/Book.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const BookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  authors: [
+    {
+      autopopulate: true,
+      type: mongoose.ObjectId,
+      ref: 'Author',
+    },
+  ],
+  owner: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+})
+
+BookSchema.plugin(require('mongoose-autopopulate'))
+
+const Book = mongoose.model('Book', BookSchema)
+
+module.exports = Book
